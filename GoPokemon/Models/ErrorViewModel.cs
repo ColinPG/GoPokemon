47b0e54fa8f33d4821b839b8231824b213cd6dde
ColinPG <carmstrong9676@conestogac.on.ca>
2021-02-23 14:27:33 -0500

    Project Created

---
+++ GoPokemon/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GoPokemon.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
