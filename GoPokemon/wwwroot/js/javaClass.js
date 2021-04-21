7bb9645ca03429b73e914780e45fe6ec5e160d0b
ColinPG <carmstrong9676@conestogac.on.ca>
2021-04-21 14:27:18 -0400

    Result page and js update.
    Update for Tensorflow model prediction on result page.

--- GoPokemon/wwwroot/js/javaClass.js
+++ GoPokemon/wwwroot/js/javaClass.js
@@ -1,22 +1,16 @@
-﻿
-    async function load() {
-      const model = await tf.loadLayersModel('model.json');
+﻿//classifier.js
+var model;
+var predResult = document.getElementById("result");
+async function initialize() {
+    model = await tf.loadLayersModel('/js/model.json');
+}
+async function predict() {
+    // action for the submit button
+    let image = document.getElementById("image");
+    let tensorImg = tf.browser.fromPixels(image).resizeNearestNeighbor([150, 150]).toFloat().expandDims();
+    
+    prediction = await model.predict(tensorImg).data();
 
-    return model;
-  };
-
-  function predict(model) {
-    // first we get the value in the input field
-      const userInput = document.getElementById('CardImage').value
-    const inputTensor = tf.tensor(userInput);  // then convert to tensor
-
-    // now lets make the prediction, we use .then because the model is a promise
-    // (this is confusing as a Python user, but useful so check it out if interested)
-    model.then(model => {
-        let result = model.predict(inputTensor);
-      result = result.round().dataSync()[0];  // round prediction and get value
-      alert(result ? "odd" : "even");
-    });
-  };
-
-  const model = load();  // load model immediately to avoid delay when user clicks 'Predict'
+    predResult.innerHTML = "This image most likely belongs to Monster Card with a " + (prediction[0] * 100) + " percent confidence.";
+}
+initialize();
